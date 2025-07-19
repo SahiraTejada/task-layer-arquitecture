adfab88e22f6d5eac1949a372abc2ad511375858
SahiraTejada <sahira2208@hotmail.com>
2025-07-19 11:05:36 -0400

    udpate

--- app/utils/exceptions.py
+++ app/utils/exceptions.py
@@ -2,9 +2,12 @@
 Custom exception classes for the application.
 """
 
+from typing import Optional
+
+
 class BaseAppException(Exception):
     """Base exception class for all application exceptions."""
-    def __init__(self, message: str, error_code: str = None):
+    def __init__(self, message: str, error_code: Optional[str] = None):
         self.message = message
         self.error_code = error_code
         super().__init__(self.message)
