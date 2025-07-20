c37532e76d3401268f7fa1287e449bd99eb1da19
SahiraTejada <sahira2208@hotmail.com>
2025-07-20 11:33:13 -0400

    remvoe

--- app/services/base.py
+++ app/services/base.py
@@ -1,4 +1,4 @@
-from typing import Generic, TypeVar, Type, List, Dict, Any, Union
+from typing import Generic, TypeVar, Type, List, Dict, Any
 from abc import ABC
 import logging
 from datetime import datetime, timezone
